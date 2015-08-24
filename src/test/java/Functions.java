9a024f32e4e5bfa243ca955f96a45a48a645f145
Prabhakaran Thatchinamoorthy <thatchinamoorthyp@dnb.com>
2015-08-24 01:17:31 -0500

    -

--- src/test/java/Functions.java
+++ src/test/java/Functions.java
@@ -3,12 +3,18 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import javax.ws.rs.core.Response;
 
 import org.junit.Test;
 
 import rx.Observable;
+import rx.Scheduler;
+import rx.Subscriber;
+import rx.Observable.OnSubscribe;
 import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.functions.Func2;
@@ -19,7 +25,7 @@ import com.example.helloworld.resources.FamilyTreeResource;
 
 public class Functions {
 	
-	private List<String> dunsList = Arrays.asList("001005003", "200443976");
+	private static List<String> dunsList = Arrays.asList("001005003", "200443976");
 	
 	Func1<String, Response> getFamilyTreeFunction = new Func1<String, Response>(){
 		public Response call(String duns) {
@@ -31,29 +37,28 @@ public class Functions {
 		System.out.println("--->: " + response);
 	};
 	
-	Action1<Object> printFileAction = new Action1<Object>(){
-		@Override
-		public void call(Object response) {
-			BufferedWriter writer = null;
+	private static void writeToFile(Object response) {
+		BufferedWriter writer = null;
+		try {
+			writer = new BufferedWriter(new FileWriter("/home/thatchinamoorthyp/Desktop/RxJava/Output", true));
+			writer.append("\n");
+			writer.append("response +++" + response);
+			
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
 			try {
-				writer = new BufferedWriter(new FileWriter("/home/thatchinamoorthyp/Desktop/RxJava/Output", true));
-				writer.append("\n");
-				writer.append("response +++" + response);
-				
-			} catch (Throwable e) {
+				writer.close();
+			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} finally{
-				try {
-					writer.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
 			}
 		}
-	};
+	}
 
+
+	
 	private Func1<? super Response, Boolean> filterGoodResults = new Func1<Response, Boolean>() {
 		public Boolean call(Response response) {
 			if(response.getStatus() == 200)
@@ -106,7 +111,7 @@ public class Functions {
 					.observeOn(Schedulers.io())
 					.map(getFamilyTreeFunction)
 					.filter(filterGoodResults)
-					.subscribe(printFileAction);
+					.subscribe(response -> writeToFile(response));
 	}
 	
 	/**
@@ -117,12 +122,26 @@ public class Functions {
 		Observable<String> dunsObservable = Observable.from(Arrays.asList("001005003", "200443976"));
 
 		dunsObservable
+					.subscribeOn(Schedulers.io())
 					.map(getFamilyTreeFunction)
 					.filter(filterGoodResults)
 					.reduce(joinAllResponses)
 					.timeInterval()
-					.doOnEach(onNotification -> System.out.println(onNotification.getValue()))
-					.subscribe();
+					.subscribe(response -> writeToFile(response));
 	}
+	
+	
+	public static void main(String [] args) {
+		Observable<String> dunsObservable = Observable.from(dunsList);
+
+		dunsObservable
+					.subscribeOn(Schedulers.computation())
+					.subscribe(response -> writeToFile(response)
+								,response -> writeToFile(response)
+								,()-> writeToFile("Completed")
+							   );
+	}
+		
+
 
 }
